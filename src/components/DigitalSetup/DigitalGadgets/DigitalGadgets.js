943ef04a5af15a96fba08635eb43497c1ee3f533
TamHOD <thomashodonnell@gmail.com>
2020-01-09 19:38:24 +0000

    add proptypes to DigitalGadgets and convert to class

--- src/components/DigitalSetup/DigitalGadgets/DigitalGadgets.js
+++ src/components/DigitalSetup/DigitalGadgets/DigitalGadgets.js
@@ -1,37 +1,45 @@
-import React from "react";
+import React, { Compornent } from "react";
+import PropTypes from "prop-types";
+
 import classes from "./BurgerIngredients.module.css";
 
-const digitalGadgets = props => {
-  let gadget = null;
+class DigitalGadgets extends Compornent {
+  render() {
+    let gadget = null;
 
-  switch (props.type) {
-    case "mixer":
-      gadget = (
-        <div className={classes.BreadTop}>
-          <div className={classes.Seeds1}></div>
-          <div className={classes.Seeds2}></div>
-        </div>
-      );
-      break;
-    case "right-hand-deck":
-      gadget = <div className={classes.BreadBottom}></div>;
-      break;
-    case "left-hand-deck":
-      gadget = <div className={classes.Meat}></div>;
-      break;
-    case "drum-machine":
-      gadget = <div className={classes.Salad}></div>;
-      break;
-    case "keyboard":
-      gadget = <div className={classes.Bacon}></div>;
-      break;
-    case "fx-module":
-      gadget = <div classname={classes.Cheese}></div>;
-      break;
-    default:
-      gadget = null;
+    switch (this.props.type) {
+      case "mixer":
+        gadget = (
+          <div className={classes.BreadTop}>
+            <div className={classes.Seeds1}></div>
+            <div className={classes.Seeds2}></div>
+          </div>
+        );
+        break;
+      case "right-hand-deck":
+        gadget = <div className={classes.BreadBottom}></div>;
+        break;
+      case "left-hand-deck":
+        gadget = <div className={classes.Meat}></div>;
+        break;
+      case "drum-machine":
+        gadget = <div className={classes.Salad}></div>;
+        break;
+      case "keyboard":
+        gadget = <div className={classes.Bacon}></div>;
+        break;
+      case "fx-module":
+        gadget = <div classname={classes.Cheese}></div>;
+        break;
+      default:
+        gadget = null;
+    }
+    return ingredient;
   }
-  return ingredient;
+}
+
+DigitalGadgets.propTypes = {
+  type: PropTypes.string.isRequired
 };
 
-export default digitalGadgets;
+export default DigitalGadgets;
