40395b8e2c56dff478c88892d10130aea465a99a
TamHOD <thomashodonnell@gmail.com>
2020-01-09 18:54:23 +0000

    setup base of digitalsetupbuilder and import to app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import Layout from "./components/Layout/Layout";
+import DigitalSetupBuilder from "./containers/DigitalSetupBuilder/DigitalSetupBuilder";
 
 function App() {
-  return <Layout />;
+  return (
+    <Layout>
+      <DigitalSetupBuilder />
+    </Layout>
+  );
 }
 
 export default App;
---
+++ src/containers/DigitalSetupBuilder/DigitalSetupBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+import Aux from "../../hoc/Aux";
+
+class DigitalSetupBuilder extends Component {
+  render() {
+    return (
+      <Aux>
+        <div>Digital Equipment</div>
+        <div>Build Controls</div>
+      </Aux>
+    );
+  }
+}
+
+export default DigitalSetupBuilder;
