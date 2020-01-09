cf38560568634443c6c9ee2a42a393f9468de69b
TamHOD <thomashodonnell@gmail.com>
2020-01-09 18:45:46 +0000

    add Layout to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import Layout from "./components/Layout/Layout";
 
 function App() {
-  return <div className="App"></div>;
+  return <Layout />;
 }
 
 export default App;
