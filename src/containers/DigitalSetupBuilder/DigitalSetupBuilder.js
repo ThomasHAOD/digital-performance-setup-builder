a198b01d1f6da77c07b94a880435efac766a13bb
TamHOD <thomashodonnell@gmail.com>
2020-01-09 22:04:43 +0000

    add positive increment handler for equpment

--- src/containers/DigitalSetupBuilder/DigitalSetupBuilder.js
+++ src/containers/DigitalSetupBuilder/DigitalSetupBuilder.js
@@ -4,6 +4,14 @@ import Aux from "../../hoc/Aux";
 import DigitalSetup from "../../components/DigitalSetup/DigitalSetup";
 import BuildControls from "../../components/DigitalSetup/BuildControls/BuildControls";
 
+const GADGETS_PRICES = {
+  deck: 200,
+  mixer: 400,
+  synth: 450,
+  fxModule: 250,
+  drumMachine: 550
+};
+
 class DigitalSetupBuilder extends Component {
   state = {
     digitalGadgets: {
@@ -12,14 +20,29 @@ class DigitalSetupBuilder extends Component {
       synth: 0,
       fxModule: 0,
       drumMachine: 0
-    }
+    },
+    totalPrice: 800
   };
+
+  addGadgetHandler = type => {
+    const oldCount = this.state.digitalGadgets[type];
+    const updatedCount = oldCount + 1;
+    const updatedGadgets = {
+      ...this.state.digitalGadgets
+    };
+    updatedGadgets[type] = updatedCount;
+    const priceAddition = GADGETS_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+    this.setState({ totalPrice: newPrice, digitalGadgets: updatedGadgets });
+  };
+
+  removeGadgetHandler = type => {};
   render() {
     return (
       <Aux>
         <DigitalSetup digitalGadgets={this.state.digitalGadgets} />
-        <div>Build Controls</div>
-        <BuildControls />
+        <BuildControls gadgetAdded={this.addGadgetHandler} />
       </Aux>
     );
   }
