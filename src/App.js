a67c59f8719c5d296372a7ee6a7924b68cb63077
TamHOD <thomashodonnell@gmail.com>
2020-01-14 11:26:13 +0000

    add router and builder and checkout routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+
 import Layout from "./hoc/Layout/Layout";
 import DigitalSetupBuilder from "./containers/DigitalSetupBuilder/DigitalSetupBuilder";
 import Checkout from "./containers/Checkout/Checkout";
@@ -6,8 +8,10 @@ import Checkout from "./containers/Checkout/Checkout";
 function App() {
   return (
     <Layout>
-      <DigitalSetupBuilder />
-      <Checkout />
+      <Switch>
+        <Route path="/checkout" component={Checkout} />
+        <Route exact path="/" component={DigitalSetupBuilder} />
+      </Switch>
     </Layout>
   );
 }
